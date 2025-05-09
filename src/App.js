aa27695187dca515b76cb803155710e3a8ef543e
Ren <Ren920@users.noreply.github.com>
2025-05-09 14:59:40 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PointsExplorer from './components/PointsExplorer';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto px-4 py-8 max-w-7xl">
+        <PointsExplorer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
